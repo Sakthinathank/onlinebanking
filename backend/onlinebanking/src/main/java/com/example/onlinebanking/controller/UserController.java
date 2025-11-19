f31f0b63cc6e64ac26b8970428c80c8395527575
sathinathank <sakthinathan@gmail.com>
2025-11-20 02:23:21 +0530

    Day 2 complete - Login API

---
+++ backend/onlinebanking/src/main/java/com/example/onlinebanking/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.example.onlinebanking.controller;
+
+import com.example.onlinebanking.dto.UserDto;
+import com.example.onlinebanking.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("register")
+
+    public String registerUser(@RequestBody UserDto userDto){
+        return userService.registerUser(userDto);
+    }
+
+
+}
